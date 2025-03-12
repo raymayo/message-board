0501ab11510fa421f3d6a31f712e9169a43a5055
raymayo <ormillada.jr@gmail.com>
2025-03-12 23:42:39 +0800

    feat: configure CORS for specific origin and add API proxy in Vite config

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express();
 const PORT =  process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:'https://kns-message-board.vercel.app',
+  credentials: true,
+}));
 // import.meta.env.VITE_PORT ||
 
 mongoose.connect(process.env.MONGO_URI, {
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,13 @@ export default defineConfig({
   plugins: [react(), tailwindcss()], 
   server: {
     historyApiFallback: true,
+    proxy: {
+      "/api": {
+        target: "https://message-board-g0z0.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
+  
 })
