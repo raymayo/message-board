4877bad886021ca9548a4dd565cc523433756f8e
raymayo <ormillada.jr@gmail.com>
2025-03-13 02:11:08 +0800

    feat: disable pointer events in Footer component for better user interaction

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='py-4 w-full mt-8 fixed bottom-0 '>
-        <h1 className='text-right pr-4 text-sm'>Based on <a href="https://sendthesong.xyz/" target='_blank' className='text-blue-500'>sendthesong.xyz</a></h1>
+    <footer className='py-4 w-full mt-8 fixed bottom-0 pointer-events-none'>
+        <h1 className='text-right pr-4 text-sm pointer-events-none'>Based on <a href="https://sendthesong.xyz/" target='_blank' className='text-blue-500'>sendthesong.xyz</a></h1>
 
         </footer>
   )
