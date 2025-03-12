a5affb9c3c003cb96138953daa16afb53f5e6870
raymayo <ormillada.jr@gmail.com>
2025-03-13 00:03:11 +0800

    refactor: clean up import statements and improve component structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
+import { Routes, Route } from 'react-router-dom';
+import AddMessage from './Components/AddMessage';
+import Header from './Components/Header.jsx';
+import Footer from './Components/Footer.jsx';
+import BrowseNotes from './Components/BrowseNotes.jsx';
+import NotePage from './Components/NotePage.jsx';
+import Home from './Components/Home.jsx';
+import Success from './Components/Success.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
-import { Routes, Route } from "react-router-dom";
-import  AddMessage  from './Components/AddMessage'
-import Header from './Components/Header.jsx'
-import Footer from './Components/Footer.jsx'
-import BrowseNotes from "./Components/BrowseNotes.jsx";
-import NotePage from "./Components/NotePage.jsx";
-import Home from "./Components/Home.jsx";
-import Success from "./Components/Success.jsx";
-import { Analytics } from "@vercel/analytics/react"
-
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-    <div className='bg-white w-screen h-screen flex flex-col justify-between items-center overflow-x-hidden'>
-      <Analytics/>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/create" element={<AddMessage />}/>
-      <Route path="/browse" element={<BrowseNotes/>}/>
-      <Route path="/notes/:id" element={<NotePage/>}/>
-      <Route path="/success" element={<Success/>}/>
-      {/* <div className='w-2/5'>
+	return (
+		<div className="bg-white w-screen h-screen flex flex-col justify-between items-center overflow-x-hidden">
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/create" element={<AddMessage />} />
+				<Route path="/browse" element={<BrowseNotes />} />
+				<Route path="/notes/:id" element={<NotePage />} />
+				<Route path="/success" element={<Success />} />
+				{/* <div className='w-2/5'>
     <AddMessage />
     </div> */}
-    </Routes>
-    <Footer/>
-    </div>
-  )
+			</Routes>
+			<Footer />
+			<Analytics />
+		</div>
+	);
 }
 
-export default App
+export default App;
