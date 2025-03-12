6c4a9b2a73189fbe2c978dbf2775e121517cbb78
raymayo <ormillada.jr@gmail.com>
2025-03-13 00:21:19 +0800

    feat: update title from "KNS Message Board" to "KNS Confession" in index.html and Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <nav className='py-2 border-b border-zinc-300 w-full sticky top-0 left-0 bg-white flex justify-center items-center'>
       <div className='w-full sm:w-4/5 md:w-4/5 lg:w-3/5 xl:w-3/5 2xl:w-3/5 flex justify-between items-center px-4'>
-        <h1 className='handwritten text-2xl'>KNS Message Board</h1>
+        <h1 className='handwritten text-2xl'>KNS Confession</h1>
         <div>
             <ul className='flex gap-4'>
                 <Link to='/' className='cursor-pointer text-sm'>Home</Link>
